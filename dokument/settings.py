cb0098b56db0de54431477d54b7461de96b9b88f
Morena <morena@localhost.localdomain>
2019-12-20 17:11:57 -0500

    bah

--- dokument/settings.py
+++ dokument/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
